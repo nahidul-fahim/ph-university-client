0ee06d7e73c5f2e6cff2d8387cd04bf68c251675
nahidul-fahim <nahidulfahim.official@gmail.com>
2024-07-08 08:56:58 +0600

    added reusable admin routes

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,6 +1,8 @@
 import { createBrowserRouter } from 'react-router-dom';
 import App from '../App';
 import { adminRoutes } from './admin.routes';
+import Register from '../pages/Register';
+import Login from '../pages/Login';
 
 const router = createBrowserRouter([
   {
