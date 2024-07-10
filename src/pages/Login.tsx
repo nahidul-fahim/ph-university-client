ceb96af8954ca2896741f4a735b795080605bc02
nahidul-fahim <nahidulfahim.official@gmail.com>
2024-07-10 09:10:33 +0600

    loggin in admin successfully

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,9 +1,48 @@
+import { Button } from "antd";
+import { useForm } from "react-hook-form";
+import { useLoginMutation } from "../redux/features/auth/authApi";
+
+
+
 const Login = () => {
 
+    const { handleSubmit, register } = useForm({
+        defaultValues: {
+            userId: "A-0001",
+            password: "admin123"
+        }
+    });
+
+    const [login, { data, error }] = useLoginMutation();
+
+    console.log("data =>", data)
+    console.log("error =>", error)
+
+
+    const onSubmit = (data) => {
+        console.log(data);
+        const userInfo = {
+            id: data?.userId,
+            password: data?.password
+        }
+
+        login(userInfo)
+    }
+
+
+
     return (
-        <div>
-            <h2>Login page</h2>
-        </div>
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <div>
+                <label htmlFor="id">ID: </label>
+                <input type="text" id="id" {...register('userId')} />
+            </div>
+            <div>
+                <label htmlFor="password">Password: </label>
+                <input type="text" id="password" {...register('password')} />
+            </div>
+            <Button htmlType="submit">Login</Button>
+        </form>
     );
 };
 
